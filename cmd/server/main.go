ef50effd298d0f2b857134865122a317db2d8451
adinahhh <42357572+adinahhh@users.noreply.github.com>
2026-04-03 20:34:22 -0500

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	gliderssh "github.com/gliderlabs/ssh"
+
+	"github.com/adinahhh/portfolio-ssh/cmd/internal/session"
+)
+
+func main() {
+	handler := session.NewHandler(session.Config{
+		AppPath: "/Users/adinah/Projects/portfolio/.venv/bin/portfolio",
+	})
+
+	srv := &gliderssh.Server{
+		Addr:    ":2222",
+		Handler: handler,
+	}
+
+	log.Println("SSH server listening on :2222")
+	log.Fatal(srv.ListenAndServe())
+}
